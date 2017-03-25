9a2d5db0ecb5918b45da30ea8ae29561f58e5ba0
Christophe VG <contact@christophe.vg>
2017-03-25 10:09:35 +0100

    retired io.socket, a normal REST service it is

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// mongo
+
 var MongoClient = require('mongodb').MongoClient;
 var collection = null;
 MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/fri3d-font", function(err, db) {
@@ -5,26 +7,33 @@ MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/fri3d-
   collection = db.collection('submissions');
 });
 
-var http = require('http').Server(app);
-var io   = require('socket.io')(http);
+// app
+
 var express = require('express')
 var app = express()
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// public
+
 app.use(express.static('static'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/form.html');
 });
 
-});
-
-io.on('connection', function(socket){
-  socket.on("submission", function(submission){
-    collection.insert(submission, {w:1}, function(err, result) {
-      socket.emit("response", err ? "error" : "ok");
-    });
+app.post('/submission', function(req, res) {
+  console.log("new submission");
+  var submission = req.body;
+  submission.ts = new Date();
+  collection.insert(submission, {w:1}, function(err, result) {
+    res.end(JSON.stringify({"result" : err ? "error" : "ok" }));
   });
 });
 
+// set up actual server
+var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
 http.listen(port, function() {
